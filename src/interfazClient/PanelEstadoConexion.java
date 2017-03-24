d788b9df8838e1461f866d6066c6b0f14640dd61
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2017-03-23 23:38:50 -0500

    Minichange

---
+++ src/interfazClient/PanelEstadoConexion.java
@@ -0,0 +1,30 @@
+package interfazClient;
+
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+
+public class PanelEstadoConexion extends JPanel{
+
+	private static final long serialVersionUID = 1L;
+	
+	private InterfazCliente principal;
+	
+	private JTextField txtEstado;
+	
+	public PanelEstadoConexion (InterfazCliente interfaz) {
+		principal = interfaz;
+		
+		setLayout(new GridLayout(1,1));		
+		setBorder(new TitledBorder("Estado de la conexión"));
+		
+		txtEstado = new JTextField("No hay conexión");
+		txtEstado.setEditable(false);
+		
+		add(txtEstado);
+	}
+
+
+}
